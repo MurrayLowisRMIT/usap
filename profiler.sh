c6d965516805cdc8498b80b128da0711ae75c3f3
Murray Lowis <s3862651@student.rmit.edu.au>
2021-11-05 03:45:36 +1100

    added profiling functions to script

--- profiler.sh
+++ profiler.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-counter=1
-while [[ "$counter" -le "10" ]]; do
-    cat /sys/class/thermal/thermal_zone0/temp >> data
-    ((counter++))
-done
+mainMenu() {
+
+}
+
+profiler() {
+    timer=1
+    while [[ "$timer" -le "100" ]]; do
+        #reads current CPU temperature in *C to 2 decimal palces
+        CPUtemp=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
+        #reads GPU temp to 2 decimal places and removes other characters
+        GPUtemp=`vcgencmd measure_temp | sed -En "s/temp=(.*)'C/\10/p"`
+        #reads current clock speed in GHz to 2 decimal places
+        clock=`echo "scale=2; $(vcgencmd measure_clock core | cut -c 14-) / 1000000000" | bc`
+        #reads free memory in gigabytes from 'free' command
+        memoryFree=`echo "scale=2; $(awk '/^Mem:/ {print $4}' <(free -b)) / 1000000000" | bc`
+        #reads used memory in gigabytes from 'free' command
+        memoryUsed=`echo "scale=2; $(awk '/^Mem:/ {print $3}' <(free -b)) / 1000000000" | bc`
+        #reads combined CPU usage for user and system as a percentage
+        CPUusage=`awk '/all/ {print $3 + $5}' <(mpstat -u)`
+
+        #writes all data to a new line in the data file
+        echo "$timer    $CPUtemp    $GPUtemp    $clock  $memoryFree $memoryUsed $CPUusage" >> data
+
+        ((timer++))
+        /bin/sleep 1
+    done
+}
