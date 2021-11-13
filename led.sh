9a97c4c16252724cae1b5480336ac9eb77fd74de
Murray Lowis <s3862651@student.rmit.edu.au>
2021-11-13 14:06:06 +1100

    minor code hygiene

--- led.sh
+++ led.sh
@@ -11,21 +11,21 @@ trap 'stopScript=1' USR2
 #set state of led trigger to "none"
 sudo sh -c "echo none > /sys/class/leds/led0/trigger"
 
-while [ $stopScript -eq 0 ]; do
+while [ ${stopScript} -eq 0 ]; do
     #record CPU uptime
     ledOn=`echo "$(awk '{print $9}' <(uptime))" | sed 's/,//'`
-    ledOff=`echo "1 - $ledOn" | bc`
+    ledOff=`echo "1 - ${ledOn}" | bc`
     #turn led on for portion of second based on CPU uptime
     sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
-    sleep $ledOn
+    sleep ${ledOn}
     #turn led off for remainder of second
     sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
-    sleep $ledOff
+    sleep ${ledOff}
 done
 
 #turn off led
 sudo sh -c "echo 1 > /sys/class/leds/led0/brightness"
 
 #revert led and trigger to starting state
-sudo sh -c "echo $ledRevert > /sys/class/leds/led0/brightness"
-sudo sh -c "echo $triggerRevert > /sys/class/leds/led0/trigger"
+sudo sh -c "echo ${ledRevert} > /sys/class/leds/led0/brightness"
+sudo sh -c "echo ${triggerRevert} > /sys/class/leds/led0/trigger"
